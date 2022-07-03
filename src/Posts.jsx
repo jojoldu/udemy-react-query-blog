38760a771e1b06f55448a50beb31c8f4b5c7378d
jojoldu <jojoldu@gmail.com>
2022-07-03 23:37:38 +0900

    3강까지 실습

---
+++ src/Posts.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import { PostDetail } from "./PostDetail";
+import {useQuery} from "react-query";
+const maxPostPage = 10;
+
+async function fetchPosts() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
+    staleTime: 2000,
+  });
+
+  if(isLoading) return <h3>Loading...</h3>
+  if(isError) return (
+    <>
+      <h3>Oops, something went wrong</h3>
+      <p>{error.toString()}</p>
+    </>
+  )
+
+  if (!data) return <div />;
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
