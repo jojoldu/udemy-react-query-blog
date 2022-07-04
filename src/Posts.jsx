6277f205acb366462ebb030a8e6114d65b66fe25
jojoldu <jojoldu@gmail.com>
2022-07-04 23:26:28 +0900

    페이지네이션

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -4,19 +4,22 @@ import { PostDetail } from "./PostDetail";
 import {useQuery} from "react-query";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNumber) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
     staleTime: 2000,
   });
 
