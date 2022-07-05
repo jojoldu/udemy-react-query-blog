36a71fc5139965d7b2959330c42e40510164db0e
jojoldu <jojoldu@gmail.com>
2022-07-05 20:10:30 +0900

    prefetchQuery 추가

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { PostDetail } from "./PostDetail";
-import {useQuery} from "react-query";
+import { useQuery, useQueryClient } from "react-query";
+
 const maxPostPage = 10;
 
 async function fetchPosts(pageNumber) {
@@ -15,23 +16,31 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const { data, isError, error, isLoading } = useQuery(
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClient]);
+
+  const {data, isError, error, isLoading} = useQuery(
     ["posts", currentPage],
     () => fetchPosts(currentPage),
     {
-    staleTime: 2000,
-  });
+      staleTime: 2000,
+      keepPreviousData: true,
+    });
 
-  if(isLoading) return <h3>Loading...</h3>
-  if(isError) return (
+  if (isLoading) return <h3>Loading...</h3>
+  if (isError) return (
     <>
       <h3>Oops, something went wrong</h3>
       <p>{error.toString()}</p>
     </>
   )
 
-  if (!data) return <div />;
+  if (!data) return <div/>;
 
   return (
     <>
@@ -47,16 +56,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={() => {
+          setCurrentPage((previous) => previous - 1);
+        }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={() => {
+          setCurrentPage((previous) => previous + 1);
+        }}>
           Next page
         </button>
+
       </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      <hr/>
+      {selectedPost && <PostDetail post={selectedPost}/>}
     </>
   );
 }
